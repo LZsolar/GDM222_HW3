d3418a70f2f957cac8d6418e42136f8b8c0193c0
LZsolar <minecraftradioevil@gmail.com>
2023-03-27 14:40:10 +0700

    Q3 thx na kub GPT-san

---
+++ Q3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Vector2 one = new Vector2(double.Parse(Console.ReadLine()),double.Parse(Console.ReadLine()));
+        Vector2 two = new Vector2(double.Parse(Console.ReadLine()),double.Parse(Console.ReadLine()));
+
+        Circle2 Cone = new Circle2(one,double.Parse(Console.ReadLine()));
+        Circle2 Ctwo = new Circle2(two,double.Parse(Console.ReadLine()));
+
+        IntersectCircles(one.x,one.y,Cone.r,two.x,two.y,Ctwo.r);
+
+    }
+
+
+    public static void IntersectCircles(double x1, double y1, double r1, double x2, double y2, double r2)
+    {
+        double dx = x2 - x1;
+        double dy = y2 - y1;
+        double d = Math.Sqrt(dx*dx + dy*dy);
+
+        if (d > r1 + r2 || d < Math.Abs(r1 - r2))
+        {
+            return ;
+        }
+
+        double a = (r1*r1 - r2*r2 + d*d) / (2*d);
+        double h = Math.Sqrt(r1*r1 - a*a);
+        double cx2 = x1 + a*dx/d;
+        double cy2 = y1 + a*dy/d;
+        double ix1 = cx2 + h*dy/d;
+        double iy1 = cy2 - h*dx/d;
+        double ix2 = cx2 - h*dy/d;
+        double iy2 = cy2 + h*dx/d;
+
+        Console.WriteLine("{0:0.00}",ix2);
+        Console.WriteLine("{0:0.00}",iy2);
+        
+        if(ix2!=ix1&&iy2!=iy1){
+            Console.WriteLine("{0:0.00}",ix1);
+            Console.WriteLine("{0:0.00}",iy1);
+        }
+        return ;
+    }
+
+}
---
+++ Q3/Vector2.cs
@@ -0,0 +1,75 @@
+public class Vector2
+{
+    //  Abscissa.
+    public double x;
+
+    //  Ordinate.
+    public double y;
+
+    public Vector2(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    //  Compute this vector magnitude.
+    public double Magnitude()
+    {
+        return Math.Sqrt((this.x * this.x) + (this.y * this.y));
+    }
+
+    public static Vector2 operator +(Vector2 u)
+    {
+        return u;
+    }
+
+    public static Vector2 operator -(Vector2 u)
+    {
+        return new Vector2(-u.x, -u.y);
+    }
+
+    public static Vector2 operator +(Vector2 u, Vector2 v)
+    {
+        return new Vector2(u.x + v.x, u.y + v.y);
+    }
+
+    public static Vector2 operator -(Vector2 u, Vector2 v)
+    {
+        return u + (-v);
+    }
+
+    //  Compute a dot product between two given vector.
+    public static double operator *(Vector2 u, Vector2 v)
+    {
+        return (u.x * v.x) + (u.y * v.y);
+    }
+
+    //  Compute a product between given scala and vector.
+    public static double operator *(Vector2 u, double k)
+    {
+        return (u.x * k) + (u.y * k);
+    }
+
+    //  Compute a product between given scala and vector.
+    public static double operator *(double k, Vector2 u)
+    {
+        return u * k;
+    }
+
+    public static Vector2 Copy(Vector2 u)
+    {
+        return new Vector2(u.x, u.y);
+    }
+
+    public override string ToString()
+    {
+        return string.Format("Vector2(x: {0}, y: {1})", this.x, this.y);
+    }
+
+    public static double findDistance(Vector2 point1,Vector2 point2){
+        double deltaX = point2.x - point1.x;
+        double deltaY = point2.y - point1.y;
+
+        return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+    }
+}
